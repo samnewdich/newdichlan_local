b7ae41a95fa51f5ed6af7d99f39bb78cc8fe1005
samnewdich <samnewdich@gmail.com>
2026-03-21 21:36:01 +0100

    corrected paystack generate controller

--- Controller/App/PaystackGenerateReservedController.php
+++ Controller/App/PaystackGenerateReservedController.php
@@ -4,6 +4,61 @@ use NewdichApis\PaystackGenerateReserved;
 use NewdichDto\AnsofraDto;
 use NewdichMiddleware\Index;
 
+header('Content-Type: application/json');
+
+// Support both form-data and JSON
+$incoming = $_POST;
+
+if (empty($incoming)) {
+    $incoming = json_decode(file_get_contents("php://input"), true);
+}
+
+$incoming = is_array($incoming) ? $incoming : [];
+
+$cleanData = [];
+$newMiddleware = new Index();
+
+foreach ($incoming as $k => $v) {
+    $cleanData[$k] = ($k === "password")
+        ? $newMiddleware->hashData($v)
+        : $newMiddleware->cleanData($v);
+}
+
+try {
+    $newDto = new AnsofraDto($cleanData);
+    $newRegister = new PaystackGenerateReserved($newDto);
+
+    $result = $newRegister->process();
+
+    // Ensure consistent output
+    if (is_string($result)) {
+        echo $result;
+    } else {
+        echo json_encode($result);
+    }
+
+} catch (\Throwable $e) {
+    echo json_encode([
+        "status" => "failed",
+        "response" => $e->getMessage()
+    ]);
+}
+
+exit;
+?>
+
+
+
+
+
+
+<?php
+/*
+namespace NewdichControllerApp;
+use NewdichApis\PaystackGenerateReserved;
+use NewdichDto\AnsofraDto;
+use NewdichMiddleware\Index;
+
 $incoming = $_POST;
 $cleanData = [];
 $newMiddleware = new Index();
@@ -21,4 +76,5 @@ $newDto = new AnsofraDto($cleanData);
 $newRegister = new PaystackGenerateReserved($newDto);
 echo $newRegister->process();
 exit;
+*/
 ?>
